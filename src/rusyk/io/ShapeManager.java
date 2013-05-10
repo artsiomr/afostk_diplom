9f5b9d55e41a5442b0d6fc2975a92edb0c6159d1
mturchyn <maksim.turchyn@gmail.com>
2013-05-10 17:33:57 +0300

    Shape file manager.

--- src/rusyk/io/ShapeManager.java
+++ src/rusyk/io/ShapeManager.java
@@ -2,13 +2,14 @@ package rusyk.io;
 
 import rusyk.figures.Shape;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 /**
@@ -16,17 +17,19 @@ import java.util.List;
  */
 public class ShapeManager {
 
-    public void save(List<Shape> shapes, OutputStream outputStream) {
+    public void save(List<Shape> shapes, File outFile) {
         try {
-            ObjectOutputStream out = new ObjectOutputStream(outputStream);
+            OutputStream fileOutputStream = new FileOutputStream(outFile);
+            ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
             out.writeObject(shapes);
         } catch (IOException e) {
             throw new IllegalArgumentException("Cannot save shapes", e);
         }
     }
 
-    public List<Shape> load(InputStream inputStream)  {
+    public List<Shape> load(File inFile)  {
         try {
+            InputStream inputStream = new FileInputStream(inFile);
             ObjectInputStream in = new ObjectInputStream(inputStream);
             return  (List<Shape>) in.readObject();
         } catch (IOException e) {
